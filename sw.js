7ace668a1f489e1c1fdf334cb0ef4ea4a90026c5
kitune2go <sajyura4810@gmail.com>
2026-08-13 06:21:35 +0900

    chore: refresh cache for upgraded trainer art

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='muscle-master-v3';
+const CACHE_NAME='muscle-master-v4';
 const APP_SHELL=['./','./index.html','./style.css','./v3.css','./app.js','./manifest.json','./assets/trainer.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim();});
